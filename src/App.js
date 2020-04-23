346e80cd14b879d49e7ba8dd56555d436a9cfb25
James Higgs <james.y.higgs@gmail.com>
2020-04-23 11:37:50 -0700

    200423

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import AdminShows from './AdminShows/AdminShows';
 import SignUpPage from './SignUpPage/SignUpPage';
 
 import BandContext from './Contexts/BandContext';
-import config from './config'
+import config from './config';
 
 class App extends Component {
   constructor() {
@@ -26,35 +26,35 @@ class App extends Component {
       addNewShow: show => {
         fetch(`${config.API_ENDPOINT}/api/shows`, {
           headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
           },
           method: 'POST',
           body: JSON.stringify({ show })
           // { show } {show:show}
           // JSON.stringify(show)
         })
-        .then(res=>res.json())
-        .then(res => {
-          console.log(res);
-          this.setState({ shows:[...this.state.shows, res ]})
-        })
-        .catch(err => console.log(err));
+          .then(res => res.json())
+          .then(res => {
+            console.log(res);
+            this.setState({ shows: [...this.state.shows, res] });
+          })
+          .catch(err => console.log(err));
       },
 
       addNewSubscriber: email => {
         fetch(`${config.API_ENDPOINT}/api/emails`, {
           headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
           },
           method: 'POST',
           body: JSON.stringify({ email })
-      })
-        .then(res => {
-        this.setState({ subscribers:[...this.state.subscribers, res ]})
-      })
-        .catch(err => console.log(err));
+        })
+          .then(res => {
+            this.setState({ subscribers: [...this.state.subscribers, res] });
+          })
+          .catch(err => console.log(err));
       }
-    }
+    };
   }
 
   componentDidMount() {
@@ -62,42 +62,42 @@ class App extends Component {
     fetch(`${config.API_ENDPOINT}/api/shows`, {
       method: 'GET',
       headers: {
-        'content-type': 'application/json',
+        'content-type': 'application/json'
       }
     })
       .then(res => {
         if (!res.ok) {
-          return res.json().then(error => Promise.reject(error))
+          return res.json().then(error => Promise.reject(error));
         }
-        return res.json()
+        return res.json();
       })
       .then(shows => {
         // still do this step
-        this.setState({ shows: shows })
+        this.setState({ shows: shows });
       })
       .catch(error => {
-        this.setState({ error })
-      })
+        this.setState({ error });
+      });
   }
 
   render() {
     return (
-      <BandContext.Provider value={ this.state }>
-        <div className = 'App'>
-          <header className = 'App-Header'>
-            <Route path='/' component={ NavBar }/> 
+      <BandContext.Provider value={this.state}>
+        <div className="App">
+          <header className="App-Header">
+            <Route path="/" component={NavBar} />
           </header>
-          <main className='Header'>
-            <Route exact path='/' component={ LandingPage }/>
-            <Route exact path='/watch' component={ WatchPage }/>
-            <Route exact path='/listen' component={ ListenPage }/>
-            <Route exact path='/shows' component={ ShowsPage }/>
-            <Route exact path='/signup' component={ SignUpPage }/>
-            <Route exact path='/admin' component={ AdminLogin }/>
-            <Route exact path='/admin_shows' component={ AdminShows }/>
+          <main className="Header">
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/watch" component={WatchPage} />
+            <Route exact path="/listen" component={ListenPage} />
+            <Route exact path="/shows" component={ShowsPage} />
+            <Route exact path="/signup" component={SignUpPage} />
+            <Route exact path="/admin" component={AdminLogin} />
+            <Route exact path="/admin_shows" component={AdminShows} />
           </main>
           <footer>
-            <Route path='/' component={ Footer }/> 
+            <Route path="/" component={Footer} />
           </footer>
         </div>
       </BandContext.Provider>
